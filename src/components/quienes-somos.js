93a048a7dd61914993cbc185f6b0a7841743fc9d
Rodrigo Cevas <rcevas@gmail.com>
2025-05-02 13:31:31 +0200

    quienes somos

---
+++ src/components/quienes-somos.js
@@ -0,0 +1,13 @@
+export function QuienesSomos() {
+  const quienesSomos = document.createElement("div");
+
+  quienesSomos.innerHTML = `
+
+    <div>
+
+    </div>
+ 
+  `;
+
+  return quienesSomos;
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import "./style.css";
 import { Header } from './components/Header.js';
 import { Inicio } from "./components/Inicio.js";
+import { QuienesSomos } from "./components/quienes-somos.js";
 
 document.querySelector("#app").append(
   Header(),
-  Inicio()
+  Inicio(),
+  QuienesSomos()
 );
