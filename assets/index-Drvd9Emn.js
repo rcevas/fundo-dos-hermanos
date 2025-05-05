2510a49b1d502c13c3d7ea4ad8b6fb232110e253
Rodrigo Cevas <rcevas@gmail.com>
2025-05-05 14:32:30 +0200

    Updates

--- assets/index-Drvd9Emn.js
+++ assets/index-Drvd9Emn.js
@@ -1,4 +1,4 @@
-(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const a of document.querySelectorAll('link[rel="modulepreload"]'))o(a);new MutationObserver(a=>{for(const i of a)if(i.type==="childList")for(const t of i.addedNodes)t.tagName==="LINK"&&t.rel==="modulepreload"&&o(t)}).observe(document,{childList:!0,subtree:!0});function c(a){const i={};return a.integrity&&(i.integrity=a.integrity),a.referrerPolicy&&(i.referrerPolicy=a.referrerPolicy),a.crossOrigin==="use-credentials"?i.credentials="include":a.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function o(a){if(a.ep)return;a.ep=!0;const i=c(a);fetch(a.href,i)}})();function n(){const e=document.createElement("nav");return e.classList.add("navbar","navbar-expand-lg","navbar-dark","bg-dark","fixed-top"),e.innerHTML=`
+(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const a of document.querySelectorAll('link[rel="modulepreload"]'))o(a);new MutationObserver(a=>{for(const i of a)if(i.type==="childList")for(const t of i.addedNodes)t.tagName==="LINK"&&t.rel==="modulepreload"&&o(t)}).observe(document,{childList:!0,subtree:!0});function n(a){const i={};return a.integrity&&(i.integrity=a.integrity),a.referrerPolicy&&(i.referrerPolicy=a.referrerPolicy),a.crossOrigin==="use-credentials"?i.credentials="include":a.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function o(a){if(a.ep)return;a.ep=!0;const i=n(a);fetch(a.href,i)}})();function c(){const e=document.createElement("nav");return e.classList.add("navbar","navbar-expand-lg","navbar-dark","bg-dark","bg-gradient","fixed-top"),e.innerHTML=`
       <div class="container">
         <a class="navbar-brand" href="#">Fundo Dos Hermanos</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -28,7 +28,7 @@
       </div>
   `,e}function r(){const e=document.createElement("header");return e.setAttribute("id","inicio"),e.innerHTML=`
     <div class="container text-white">
-      <p class="display-6">TURISMO CINEGÉTICOEN LA PATAGONIACHILENA</p>
+      <p class="display-6">TURISMO CINEGÉTICOEN LA PATAGONIA CHILENA</p>
       <p class="display-6">Explora lo salvaje</p>
       <p class="lead">Experiencias premium de caza y pesca</p>
       <a href="#contacto" class="btn btn-outline-light btn-lg mt-4">Reservar ahora</a>
@@ -41,7 +41,7 @@
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <div class="col-lg-6 text-center">
-          <img src="https://placehold.co/600x400" alt="About Us" class="img-fluid rounded my-2">
+          <img src="https://placehold.co/600x400" alt="About Us" class="img-fluid rounded shadow my-2">
         </div>
       </div>
     </div> 
@@ -51,7 +51,7 @@
       <p class="text-center mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       <div class="row align-items-center ">
         <div class="col-lg-6 text-center">
-          <img src="https://placehold.co/600x400" alt="About Us" class="img-fluid rounded my-2">
+          <img src="https://placehold.co/600x400" alt="About Us" class="img-fluid rounded shadow my-2">
         </div>
         <div class="col-lg-6 text-center text-lg-start my-3"> 
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -62,7 +62,7 @@
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <div class="col-lg-6 text-center my-3">
-          <img src="https://placehold.co/600x400" alt="About Us" class="img-fluid rounded">
+          <img src="https://placehold.co/600x400" alt="About Us" class="img-fluid rounded shadow my-2">
         </div>
       </div>
     </div>
@@ -85,7 +85,7 @@
       <h2 class="text-center mb-5">Servicios</h2>
       <div class="row g-4">
         <div class="col-lg-6">
-          <div class="card h-100 shadow bg-dark text-white">
+          <div class="card h-100 shadow text-white shadow">
             <img src="https://placehold.co/400x400" class="card-img" alt="Expediciones de caza">
             <div class="card-img-overlay text-center">
               <h3 class="card-title">Expediciones de caza</h3>
@@ -96,7 +96,7 @@
           </div>
         </div>
         <div class="col-lg-6"> 
-          <div class="card h-100 shadow bg-dark text-white">
+          <div class="card h-100 shadow text-white shadow">
             <img src="https://placehold.co/400x400" class="card-img" alt="Expediciones de pesca">
             <div class="card-img-overlay text-center">
               <h3 class="card-title">Expediciones de pesca</h3>
@@ -111,7 +111,7 @@
       <h2 class="text-center mb-5">Precios</h2>
       <div class="row g-4">
         <div class="col-lg-6">
-          <div class="card text-center border-primary h-100">
+          <div class="card text-center border-primary h-100 shadow">
             <div class="card-header bg-primary text-white">
               Paquete de caza
             </div>
@@ -121,7 +121,7 @@
                 <li>Por persona - diario</li>
                 <li>Guía experto de la zona</li>
                 <li>Equipo proporcionado</li>
-                <li>Estadía en puestode montaña</li>
+                <li>Estadía en puesto de montaña</li>
                 <li>Comidas incluidas</li> 
                 <li>Apoyo de caballares</li>                 
               </ul>
@@ -129,7 +129,7 @@
           </div>
         </div>
         <div class="col-lg-6">
-          <div class="card text-center border-success h-100">
+          <div class="card text-center border-success h-100 shadow">
             <div class="card-header bg-success text-white">
               Paquete de pezca
             </div>
@@ -139,7 +139,7 @@
                 <li>Por persona - diario</li>
                 <li>Guía experto de la zona</li>
                 <li>Todo el equipo de pesca proporcionado</li>  
-                <li>Estadía en puestode montaña</li>              
+                <li>Estadía en puesto de montaña</li>              
                 <li>Comidas incluidas</li>
                 <li>Apoyo de caballares</li>     
               </ul>
@@ -149,7 +149,7 @@
       </div>
       <p class="text-center mt-4">Forma de pago mediante transferencia bancaria, 50% al momento de efectuar la reserva.</p>
     </div>
-  `,e}function v(){const e=document.createElement("section");return e.setAttribute("id","contacto"),e.classList.add("bg-dark","text-white"),e.innerHTML=`
+  `,e}function v(){const e=document.createElement("section");return e.setAttribute("id","contacto"),e.classList.add("bg-dark","text-white","bg-gradient"),e.innerHTML=`
     <div class="container">      
       <div class="row g-3">
         <div class="col-lg-6 text-center text-lg-start mb-4">
@@ -157,15 +157,15 @@
           <span><i class="bi bi-geo-alt"></i></span>
           <span class="ms-2">Villa Mañihuales, Aysén, Chile</span><br>
           <span><i class="bi bi-telephone"></i></span>
-          <a class="ms-2 text-white" href="tel:+56000000000">+56 000000000</a><br>
+          <a class="ms-2 text-white text-decoration-none" href="tel:+56000000000">+56 000000000</a><br>
           <span><i class="bi bi-envelope-at"></i></span>
-          <a class="ms-2 text-white" href="mailto:fundodoshermanos@gmail.com">fundodoshermanos@gmail.com</a>
+          <a class="ms-2 text-white text-decoration-none" href="mailto:fundodoshermanos@gmail.com">fundodoshermanos@gmail.com</a>
         </div>
         <div class="col-lg-6">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22501.361211256622!2d-72.17508550989648!3d-45.173528894521745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbdf315da82e2d6f3%3A0x68b0470258a36370!2zVmlsbGEgTWHDsWlodWFsZXMsIEF5c8OpbiwgQ2hpbGU!5e0!3m2!1ses!2ses!4v1746440601853!5m2!1ses!2ses" width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
     </div>
-  `,e}function b(){const e=document.createElement("footer");return e.classList.add("bg-black","text-light","text-center","p-3"),e.innerHTML=`
+  `,e}function g(){const e=document.createElement("footer");return e.classList.add("bg-black","bg-gradient","text-light","text-center","p-3"),e.innerHTML=`
     <small>© 2025 Fundo Dos Hermanos. All rights reserved.</small>
-  `,e}document.querySelector("#app").append(n(),r(),d(),u(),m(),p(),v(),b());const g=document.querySelectorAll(".navbar-nav .nav-link"),l=document.querySelector(".navbar-collapse");g.forEach(e=>{e.addEventListener("click",()=>{l.classList.contains("show")&&new bootstrap.Collapse(l).hide()})});
+  `,e}document.querySelector("#app").append(c(),r(),d(),u(),m(),p(),v(),g());const b=document.querySelectorAll(".navbar-nav .nav-link"),l=document.querySelector(".navbar-collapse");b.forEach(e=>{e.addEventListener("click",()=>{l.classList.contains("show")&&new bootstrap.Collapse(l).hide()})});
