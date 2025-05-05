d1c1fca650ebcdf50cd1fe053ce4b1768d227340
Rodrigo Cevas <rcevas@gmail.com>
2025-05-05 13:28:17 +0200

    closing responsive menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ export function NavBar() {
   nav.innerHTML = `
       <div class="container">
         <a class="navbar-brand" href="#">Fundo Dos Hermanos</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
--- src/main.js
+++ src/main.js
@@ -20,3 +20,14 @@ document
     Contacto(),
     Footer()
   );
+
+const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+const navbarCollapse = document.querySelector(".navbar-collapse");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (navbarCollapse.classList.contains("show")) {
+      new bootstrap.Collapse(navbarCollapse).hide();
+    }
+  });
+});
