49f1b1a8215896be715d58bdab9ce87d54dc303f
Rodrigo Cevas <rcevas@gmail.com>
2025-05-05 11:12:57 +0200

    quienes somos

--- src/components/quienes-somos.js
+++ src/components/quienes-somos.js
@@ -1,12 +1,21 @@
 export function QuienesSomos() {
-  const quienesSomos = document.createElement("div");
+
+  const quienesSomos = document.createElement('section');
+  quienesSomos.setAttribute("id", "quienes_somos");
+  quienesSomos.classList.add("bg-light");
 
   quienesSomos.innerHTML = `
-
-    <div>
-
-    </div>
- 
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-6">
+          <h2>Quienes Somos</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="col-md-6">
+          <img src="https://placehold.co/600x400" alt="About Us" class="img-fluid rounded">
+        </div>
+      </div>
+    </div> 
   `;
 
   return quienesSomos;
