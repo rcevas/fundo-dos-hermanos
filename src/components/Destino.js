eb4f356ce6d1673808d242ffb0d5465b1e0bc661
Rodrigo Cevas <rcevas@gmail.com>
2025-05-05 11:16:10 +0200

    breakpoints

--- src/components/Destino.js
+++ src/components/Destino.js
@@ -8,18 +8,18 @@ export function Destino() {
       <h2 class="text-center mb-4">Destino</h2>
       <p class="text-center mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       <div class="row align-items-center py-5">
-        <div class="col-md-6">
+        <div class="col-lg-6">
           <img src="https://placehold.co/600x400" alt="About Us" class="img-fluid rounded">
         </div>
-        <div class="col-md-6"> 
+        <div class="col-lg-6"> 
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
       </div>
       <div class="row align-items-center py-5">
-        <div class="col-md-6">
+        <div class="col-lg-6">
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
-        <div class="col-md-6">
+        <div class="col-lg-6">
           <img src="https://placehold.co/600x400" alt="About Us" class="img-fluid rounded">
         </div>
       </div>
--- src/components/quienes-somos.js
+++ src/components/quienes-somos.js
@@ -7,11 +7,11 @@ export function QuienesSomos() {
   quienesSomos.innerHTML = `
     <div class="container">
       <div class="row align-items-center">
-        <div class="col-md-6">
+        <div class="col-lg-6">
           <h2>Quienes Somos</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
-        <div class="col-md-6">
+        <div class="col-lg-6">
           <img src="https://placehold.co/600x400" alt="About Us" class="img-fluid rounded">
         </div>
       </div>
