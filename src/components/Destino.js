45a552401a42cf6b89ef405cfae7b0980193ba2f
Rodrigo Cevas <rcevas@gmail.com>
2025-05-06 08:30:36 +0200

    carrousel controls

--- src/components/Destino.js
+++ src/components/Destino.js
@@ -36,6 +36,14 @@ export function Destino() {
           <img src="https://placehold.co/1920x800" class="d-block w-100" alt="...">
         </div>
       </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
   `;
 
