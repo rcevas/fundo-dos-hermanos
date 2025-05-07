dd89d88a9d00eba02222c343d6e44787a586fb06
Rodrigo Cevas <rcevas@gmail.com>
2025-05-07 11:11:53 +0200

    destino carousel

--- src/components/Destino.js
+++ src/components/Destino.js
@@ -50,48 +50,34 @@ export function Destino() {
         </div>
       </div>
     </div>
+
     <p class="display-6 text-center text-success my-4">
       ¡Vive la experiencia en Fundo Dos Hermanos!
     </p>
+
+
     <div id="carousel_destino" class="carousel slide mt-5" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carousel_destino" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carousel_destino" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carousel_destino" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img
-            src="https://placehold.co/1920x800"
-            class="d-block w-100"
-            alt="..."
-          />
+          <img src="https://placehold.co/1920x720" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-          <img
-            src="https://placehold.co/1920x800"
-            class="d-block w-100"
-            alt="..."
-          />
+          <img src="https://placehold.co/1920x720" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-          <img
-            src="https://placehold.co/1920x800"
-            class="d-block w-100"
-            alt="..."
-          />
+          <img src="https://placehold.co/1920x720" class="d-block w-100" alt="...">
         </div>
       </div>
-      <button
-        class="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="prev"
-      >
+      <button class="carousel-control-prev" type="button" data-bs-target="#carousel_destino" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
       </button>
-      <button
-        class="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="next"
-      >
+      <button class="carousel-control-next" type="button" data-bs-target="#carousel_destino" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
       </button>
